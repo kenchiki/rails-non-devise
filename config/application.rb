25fac5d4e7ec1a0b8fd33f20e3180430e5e91efb
nagaikenji <nagai-kj@nagai-galaxy.com>
2019-07-07 13:41:44 +0900

    setting

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,19 @@ module RailsNonDevise
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
+    config.generators do |g|
+      g.jbuilder false
+    end
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false
+    end
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
